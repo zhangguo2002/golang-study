31cfbed47950a322537d98e2cc45aefe77cb141d
zhangguo2002 <1792203237@qq.com>
2026-04-12 13:52:12 +0800

    feat:unsafe.Pointer与uintptr有什么区别和联系、Go语言普通指针和unsafe.Pointer有什么区别、Go语言中的_的作用、Go语言的多返回值是怎么实现的、如何知道一个对象是分配在栈上还是堆上、Go语言函数传参是值类型还是引用类型、2个nil可能不相等吗、2个interface可以比较吗、init()函数是什么时候执行的、go语言中，空strct{}有什么用、go语言中空struct{}占用空间吗

--- 类禅道管理项目系统/backend/project-common/jwts/jwts_test.go
+++ 类禅道管理项目系统/backend/project-common/jwts/jwts_test.go
@@ -1,8 +1,6 @@
 package jwts
 
-import "testing"
-
-func TestParseToken(t *testing.T) {
-	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzExMjcxOTEsInRva2VuIjoiMTAxNCJ9.nc-i2-36CIlkhTEzxV0L3JDUXVD68ASefnl33PU9PcE"
-	ParseToken(tokenString, "msproject")
-}
+// func TestParseToken(t *testing.T) {
+// 	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzExMjcxOTEsInRva2VuIjoiMTAxNCJ9.nc-i2-36CIlkhTEzxV0L3JDUXVD68ASefnl33PU9PcE"
+// 	ParseToken(tokenString, "msproject")
+// }
