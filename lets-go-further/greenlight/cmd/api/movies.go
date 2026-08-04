f326c2d980f606ec6b220b148f48096ce0273603
zhangguo2002 <1792203237@qq.com>
2026-08-04 13:15:51 +0800

    feat:Parsing JSON Requests

--- lets-go-further/greenlight/cmd/api/movies.go
+++ lets-go-further/greenlight/cmd/api/movies.go
@@ -8,10 +8,19 @@ import (
 	"greenlight.alexedwards.net/internal/data"
 )
 
-// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
-// return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"` // Make this field a data.Runtime type.
+		Genres  []string     `json:"genres"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
