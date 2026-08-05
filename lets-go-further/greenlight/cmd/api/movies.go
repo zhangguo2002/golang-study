ce9837dfd10e9a5554d2fb76f6befb191647fb51
zhangguo2002 <1792203237@qq.com>
2026-08-05 15:39:59 +0800

    feat:AdvancedCRUDOperations

--- lets-go-further/greenlight/cmd/api/movies.go
+++ lets-go-further/greenlight/cmd/api/movies.go
@@ -78,63 +78,87 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	}
 }
 
-//	func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
-//		// Extract the movie ID from the URL .
-//		id, err := app.readIDParam(r)
-//		if err != nil {
-//			app.notFoundResponse(w, r)
-//			return
-//		}
-//		// Fetch the existing movie record from the database , sending a 404 Not Found
-//		// response to the client if we couldn 't find a matching record.
-//		movie, err := app.models.Movies.Get(id)
-//		if err != nil {
-//			switch {
-//			case errors.Is(err, data.ErrRecordNotFound):
-//				app.notFoundResponse(w, r)
-//			default:
-//				app.serverErrorResponse(w, r, err)
-//			}
-//			return
-//		}
-//		// Declare an input struct to hold the expected data from the client.
-//		var input struct {
-//			Title   string       `json:"title "`
-//			Year    int32        `json:"year "`
-//			Runtime data.Runtime `json:"runtime "`
-//			Genres  []string     `json:"genres "`
-//		}
-//		// Read the JSON request body data into the input struct.
-//		err = app.readJSON(w, r, &input)
-//		if err != nil {
-//			app.badRequestResponse(w, r, err)
-//			return
-//		}
-//		// Copy the values from the request body to the appropriate fields of the movie
-//		// record.
-//		movie.Title = input.Title
-//		movie.Year = input.Year
-//		movie.Runtime = input.Runtime
-//		movie.Genres = input.Genres
-//		// Validate the updated movie record , sending the client a 422 Unprocessable Entity
-//		// response if any checks fail.
-//		v := validator.New()
-//		if data.ValidateMovie(v, movie); !v.Valid() {
-//			app.failedValidationResponse(w, r, v.Errors)
-//			return
-//		}
-//		// Pass the updated movie record to our new Update () method.
-//		err = app.models.Movies.Update(movie)
-//		if err != nil {
-//			app.serverErrorResponse(w, r, err)
-//			return
-//		}
-//		// Write the updated movie record in a JSON response .
-//		err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
-//		if err != nil {
-//			app.serverErrorResponse(w, r, err)
-//		}
-//	}
+// func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+// 	id, err := app.readIDParam(r)
+// 	if err != nil {
+// 		app.notFoundResponse(w, r)
+// 		return
+// 	}
+// 	movie, err := app.models.Movies.Get(id)
+// 	if err != nil {
+// 		switch {
+// 		case errors.Is(err, data.ErrRecordNotFound):
+// 			app.notFoundResponse(w, r)
+// 		default:
+// 			app.serverErrorResponse(w, r, err)
+// 		}
+// 		return
+// 	}
+// 	// If the request contains a X-Expected-Version header , verify that the movie
+// 	// version in the database matches the expected version specified in the header .
+// 	if r.Header.Get("X-Expected-Version") != "" {
+// 		if strconv.FormatInt(int64(movie.Version), 32) != r.Header.Get("X-Expected-Version") {
+// 			app.editConflictResponse(w, r)
+// 			return
+// 		}
+// 	}
+// 	// Use pointers for the Title , Year and Runtime fields .
+// 	var input struct {
+// 		Title   *string       `json:"title "`
+// 		Year    *int32        `json:"year "`
+// 		Runtime *data.Runtime `json:"runtime "`
+// 		Genres  []string      `json:"genres "`
+// 	}
+// 	// Decode the JSON as normal.
+// 	err = app.readJSON(w, r, &input)
+// 	if err != nil {
+// 		app.badRequestResponse(w, r, err)
+// 		return
+// 	}
+// 	// If the input.Title value is nil then we know that no corresponding "title " key/
+// 	// value pair was provided in the JSON request body . So we move on and leave the
+// 	// movie record unchanged. Otherwise , we update the movie record with the new title
+// 	// value . Importantly , because input.Title is a now a pointer to a string , we need
+// 	// to dereference the pointer using the * operator to get the underlying value
+// 	// before assigning it to our movie record.
+// 	if input.Title != nil {
+// 		movie.Title = *input.Title
+// 	}
+// 	// We also do the same for the other fields in the input struct.
+// 	if input.Year != nil {
+// 		movie.Year = *input.Year
+// 	}
+// 	if input.Runtime != nil {
+// 		movie.Runtime = *input.Runtime
+// 	}
+// 	if input.Genres != nil {
+// 		movie.Genres = input.Genres // Note that we don 't need to dereference a slice .
+// 	}
+// 	v := validator.New()
+// 	if data.ValidateMovie(v, movie); !v.Valid() {
+// 		app.failedValidationResponse(w, r, v.Errors)
+// 		return
+// 	}
+
+// 	// Intercept any ErrEditConflict error and call the new editConflictResponse()
+// 	// helper .
+// 	err = app.models.Movies.Update(movie)
+// 	if err != nil {
+// 		switch {
+// 		case errors.Is(err, data.ErrEditConflict):
+// 			app.editConflictResponse(w, r)
+// 		default:
+// 			app.serverErrorResponse(w, r, err)
+// 		}
+// 		return
+// 	}
+// 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+// 	if err != nil {
+// 		app.serverErrorResponse(w, r, err)
+// 	}
+
+// }
+
 func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
 	// Extract the movie ID from the URL .
 	id, err := app.readIDParam(r)
